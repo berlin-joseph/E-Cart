bfabdd9a49b09bbd089254dd049fa8253454f451
Berlin_Joseph <l.berlinjoe@hotmail.com>
2023-07-20 12:23:45 +0530

    modified:   server/controller/productController.js 	modified:   server/routes/productsRoute.js

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -54,3 +54,21 @@ exports.updateProducts = async (req, res, next) => {
     });
   }
 };
+
+// Delete Products - api/v1/products/:id
+exports.deleteProducts = async (req, res, next) => {
+  let product = await productModel.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  } else {
+    await productModel.findOneAndRemove();
+    res.status(201).json({
+      success: true,
+      message: "Product Deleted Successfully",
+    });
+  }
+};
--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -4,10 +4,15 @@ const {
   createProducts,
   getSingleProduct,
   updateProducts,
+  deleteProducts,
 } = require("../controller/productController");
 const router = express.Router();
 
 router.route("/products").get(getProducts).post(createProducts);
-router.route("/products/:id").get(getSingleProduct).put(updateProducts);
+router
+  .route("/products/:id")
+  .get(getSingleProduct)
+  .put(updateProducts)
+  .delete(deleteProducts);
 
 module.exports = router;
